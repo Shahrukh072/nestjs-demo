41f2f7fa5581d66415ae9421e1a6c10f27d50f57
shahrukh khan <shahrukh.khan072@gmail.com>
2023-08-09 17:41:46 +0530

    fix a bug

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { JobsController } from "./controllers/jobs.controller";
 import { OfficeController } from "./controllers/office.controller";
 import { JobsService } from "./services/jobs.service";
-import { CacheStoreModule, StoreType } from "src/cache-store";
+// import { CacheStoreModule, StoreType } from "src/cache-store";
 import { JobsApplicationsModule } from "./jobs-applications/jobs-applications.module";
 
 @Module({
@@ -10,12 +10,12 @@ import { JobsApplicationsModule } from "./jobs-applications/jobs-applications.mo
   // because `JobsModule` exports the `JobsApplicationsModule`
   imports: [
 
-     // create a dynamic module with below option
-     CacheStoreModule.register({
-      storeName: "jobs",
-      storeType: StoreType.FILE,
-      storeDir: __dirname, // dist folder
-    }),
+    //  // create a dynamic module with below option
+    //  CacheStoreModule.register({
+    //   storeName: "jobs",
+    //   storeType: StoreType.FILE,
+    //   storeDir: __dirname, // dist folder
+    // }),
 
     JobsApplicationsModule,
   ],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,16 +2,16 @@ import { Module } from "@nestjs/common";
 import { AccountsController } from "./controllers/accounts.controller";
 import { UsersController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
-import { CacheStoreModule } from "src/cache-store";
+// import { CacheStoreModule } from "src/cache-store";
 
 
 @Module({
 
   imports: [
-    // create a dynamic module with below option
-    CacheStoreModule.register({
-      storeName: "users",
-    }),
+    // // create a dynamic module with below option
+    // CacheStoreModule.register({
+    //   storeName: "users",
+    // }),
   ],
   
     controllers: [UsersController, AccountsController],
